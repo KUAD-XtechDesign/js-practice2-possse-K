74fae61b9f4ce6d0dc444cddb9e1ae5727b978ce
possse-K <68629163+possse-K@users.noreply.github.com>
2020-10-23 16:35:56 +0900

    ai

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,47 @@
 $(function(){
 
+    let txt1 = "おはようございます！";
+    let txt2 = "おげんきですか？";
+    let num1 = 20;
+    let num2 = 32;
 
+    function seyHellow(){
+        $("#content").text("こんにちは！");
+    }
+
+    function saySomething(txt){
+        $("#content").text(txt);
+    }
+
+    function samTow(arg1,arg2){
+        $("#content").append(arg1 + arg2);
+    }
+    　　
+    $("#button01").on("click",function(){
+      seyHellow();
+    })
+    
+    $("#button02").on("click",function(){
+      saySomething(txt1);
+    })
+    
+    $("#button03").on("click",function(){
+       //$("#content").text(num1 + num2);
+        samTow(txt1,txt2);
+    })
+    
+    $("#button04").on("click",function(){
+      //$("#content").text(num1 + num2);
+      samTow(num1,num2);
+    })
+    
+    // + - * /
+    
+    $("#button05").on("click",function(){
+      samTow(txt1,txt2);
+      samTow(num1,num2);
+    })
+   
 
 
 })
--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,5 +1,30 @@
 $(function(){
 
+let hako = 0;
 
+function checkBox(){
+    if(hako > 5){
+        $("#content").text(hako + "個はいっています。箱はいっぱいです。空にします。");
+        hako = 0;
+    }else if(hako <= 0){
+        hako = 0;
+        $("#content").text(hako+"個。はいっていません");
+    }else{
+        $("#content").text(hako+"個はいっています。まだはいります。");
+    }
+}
+
+$("#button01").on("click",function(){
+    checkBox()
+})
+
+$("#button02").on("click",function(){
+    hako++;
+    checkBox()
+})
+$("#button03").on("click",function(){
+    hako--;
+    checkBox()
+})
 
 })
